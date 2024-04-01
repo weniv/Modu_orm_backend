eb45d11a488ee8b7532943166037b20ae8532df8
paullabkorea <paullabkorea@gmail.com>
2024-04-01 11:55:45 +0900

    0401

--- Django/0401/viewset/posts/views.py
+++ Django/0401/viewset/posts/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from .serializers import PostSerializer, CommentSerializer
 from .models import Comment, Post, Like
 from rest_framework import permissions, views, response, status
@@ -10,8 +11,27 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+    def list(self, request, *args, **kwargs):
+        self.permission_classes = [permissions.AllowAny]
+        return super().list(request, *args, **kwargs)
+
+    @action(detail=True, methods=["get"])
+    def sampleone(self, request, pk=None):
+        """
+        detail=True: 특정 게시물에 대한 작업
+        detail=False: 모든 게시물에 대한 작업
+        /posts/{pk}/sampleone/: 개별 게시물의 제목과 내용을 반환하는 엔드포인트 (detail=True)
+        """
+        data = {"title": "hello", "content": "world"}
+        return response.Response(data)
+
+    @action(detail=False, methods=["get"])
+    def sampletwo(self, request):
+        """
+        /posts/sampletwo/: 모든 게시물의 제목과 작성자 이름을 반환하는 엔드포인트 (detail=False)
+        """
+        data = [{"title": "hello 2", "author": "world 2"}]
+        return response.Response(data)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
